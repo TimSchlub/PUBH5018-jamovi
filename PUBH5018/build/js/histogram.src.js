e049e9b23e13c4513576ff74094ef616396be327
TimSchlub <41557818+TimSchlub@users.noreply.github.com>
2020-05-18 21:11:43 +1000

    Add files via upload
    Fixed color scheme for histogram
    Fixed errors when no variable selected in data cleaning

---
+++ PUBH5018/build/js/histogram.src.js
@@ -0,0 +1,80 @@
+
+// This file is an automatically generated and should not be edited
+
+'use strict';
+
+const options = [{"name":"data","type":"Data"},{"name":"dep","title":"Dependent Variable","type":"Variable"},{"name":"anchor","title":"Anchor","type":"Number","default":0},{"name":"binwidth","title":"Bin Width","type":"Number","default":1}];
+
+const view = function() {
+    
+    this.handlers = { }
+
+    View.extend({
+        jus: "3.0",
+
+        events: [
+
+	]
+
+    }).call(this);
+}
+
+view.layout = ui.extend({
+
+    label: "Histogram",
+    jus: "3.0",
+    type: "root",
+    stage: 0, //0 - release, 1 - development, 2 - proposed
+    controls: [
+		{
+			type: DefaultControls.VariableSupplier,
+			typeName: 'VariableSupplier',
+			persistentItems: false,
+			stretchFactor: 1,
+			controls: [
+				{
+					type: DefaultControls.TargetLayoutBox,
+					typeName: 'TargetLayoutBox',
+					label: "Dependent Variable",
+					controls: [
+						{
+							type: DefaultControls.VariablesListBox,
+							typeName: 'VariablesListBox',
+							name: "dep",
+							maxItemCount: 1,
+							isTarget: true
+						}
+					]
+				}
+			]
+		},
+		{
+			type: DefaultControls.LayoutBox,
+			typeName: 'LayoutBox',
+			margin: "large",
+			controls: [
+				{
+					type: DefaultControls.TextBox,
+					typeName: 'TextBox',
+					name: "anchor",
+					format: FormatDef.number
+				}
+			]
+		},
+		{
+			type: DefaultControls.LayoutBox,
+			typeName: 'LayoutBox',
+			margin: "large",
+			controls: [
+				{
+					type: DefaultControls.TextBox,
+					typeName: 'TextBox',
+					name: "binwidth",
+					format: FormatDef.number
+				}
+			]
+		}
+	]
+});
+
+module.exports = { view : view, options: options };
